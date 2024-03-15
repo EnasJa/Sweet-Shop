b0089b3efeba650315a8a4fdbccfc80924539b4a
enas <enasabo2811@gmail.com>
2024-03-15 13:07:50 +0200

    Modification of customer properties
    1-main page
    2- customer registration
    3-connecting the database

---
+++ Sweet_Shop/Sweet_Shop/Controllers/CustomerController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Sweet_Shop.Models;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Sweet_Shop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IConfiguration _configuration;
+        public string connectionString = "";
+        public CustomerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("connectStringforCustomer");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //[Route("")]
+        public IActionResult MainPage()
+        {
+            return View("MainPage");
+        }
+
+
+
+
+        //public IActionResult CustomerRegistration()
+        //{
+        //    CustomerModel customer = new CustomerModel();
+        //    return View("CustomerRegistration", customer);
+        //}
+
+        [Route("")]
+
+        public IActionResult CustomerRegistration(CustomerModel customer)
+        {
+            if (ModelState.IsValid)
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Customer (CustomerID, FirstName, LastName, Email, Phone, CAddress) VALUES (@value1,@value2, @value3, @value4, @value5, @value6)";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@value1", 57968);
+                    command.Parameters.AddWithValue("@value2", customer.FirstName);
+                    command.Parameters.AddWithValue("@value3", customer.LastName);
+                    command.Parameters.AddWithValue("@value4", customer.Email); 
+                    command.Parameters.AddWithValue("@value5", customer.Phone);
+                    command.Parameters.AddWithValue("@value6", customer.CAddress);
+                
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        // Insertion successful
+                        return View("MainPage");
+                    }
+                    else
+                    {
+                        // Insertion failed
+                        ViewBag.Message = "Failed to insert customer data.";
+                        return View("CustomerRegistration", customer);
+                    }
+                }
+            }
+            return View("CustomerRegistration");
+        }
+    }
+    }
---
+++ Sweet_Shop/Sweet_Shop/Models/CustomerModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sweet_Shop.Models
+{
+    public class CustomerModel
+    {
+       
+        [Required]
+        public int CustomerID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string CAddress { get; set; }
+        
+        public CustomerModel()
+        {
+            CustomerID = 2565364;
+            FirstName = "enas";
+            LastName = "jaber";
+            Email = "wejhdfj";
+            Phone = "3546657";
+            CAddress = "dgdh";
+        }
+    }
+}
